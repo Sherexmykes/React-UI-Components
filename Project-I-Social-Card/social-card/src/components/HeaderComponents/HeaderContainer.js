f6ea7d2d337057fc74f4e3c6badebe61ad824c06
Sheila R Moore <sherexmykes@github.com>
2019-05-28 21:47:23 -0400

    ImageThumbnail and Setup

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContainer =() => { 
+    return 
+    <ImageThumbnail />;
+    <HeaderTitle />;
+    <HeaderContent />;
+}
+export default HeaderContainer;
