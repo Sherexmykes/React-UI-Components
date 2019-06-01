129476a996eec0bb50cfdbcb7ba3e3c6b71bf586
Sheila R Moore <sherexmykes@github.com>
2019-06-01 14:37:50 -0400

    calculator is fully styled

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Display.css';
+
+
+const CalculatorDisplay = props => {
+    return(
+      <div className={`container ${props.className}`}>
+        <h1>0</h1>
+      </div>
+    )
+  
+  
+  }
+  
+
+export default CalculatorDisplay; 
