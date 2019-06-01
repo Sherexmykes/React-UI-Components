237ef003d257f357b765710d8c419d152e2315ca
Sheila R Moore <sherexmykes@github.com>
2019-06-01 15:15:03 -0400

    added href to make reactjs.org clickable

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -5,8 +5,9 @@ const CardContent = () => {
       <div className="card-content">
         <h2>Get started with React</h2>
         <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-        <p>reactjs.org</p>
+        <a href="http://reactjs.org"> reactjs.org</a>
       </div>
+   
     )
   }
 
